0e7fe77877d04ef344171f94dea374e3ecfe2828
zee <zee@zees-MacBook-Air.local>
2025-12-16 22:19:40 +0000

    Initial commit: Vite + React + TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,5 @@
+function Message(){
+    return <dev><h1>    Hello World  </h1> </dev>  ;
+}
+
+export default Message;
